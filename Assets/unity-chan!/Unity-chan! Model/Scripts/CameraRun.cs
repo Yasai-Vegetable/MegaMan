c9bc51deb815d149390a39fe40203593737dd9f7
Yasai-Vegetable <vegetable.yasai@gmail.com>
2021-07-18 22:44:11 +0900

    ユニティちゃんが歩いて、ユニティちゃんにカメラが追従するように実装

---
+++ Assets/unity-chan!/Unity-chan! Model/Scripts/CameraRun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRun : MonoBehaviour
+{
+
+    public GameObject unitychan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(unitychan.transform.position.x, 1, -10);   
+    }
+}
